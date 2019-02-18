8316ba38052a2d6d869ed627bd9a6cb3c1cf5c8c
zhufeng8407 <zhufeng.zhu@163.com>
2019-02-18 14:16:36 +0800

    策略模式快速排序代码优化提交 zhufeng

---
+++ src/main/java/com/shengsi/design/pattern/behavioral/strategy/QuickSortStrategyV2.java
@@ -0,0 +1,50 @@
+package com.shengsi.design.pattern.behavioral.strategy;
+
+import java.util.Random;
+
+public class QuickSortStrategyV2 implements Strategy {
+
+    @Override
+    public void sort(int[] arr) {
+        quickSort(arr, 0, arr.length - 1);
+    }
+
+    private void quickSort(int[] arr, int l, int r) {
+
+        if (l >= r)
+            return;
+        int p = partition(arr, l, r);
+        quickSort(arr, l, p - 1);
+        quickSort(arr, p + 1, r);
+    }
+
+    private int partition(int[] arr, int l, int r) {
+
+        int k = new Random().nextInt(r - l + 1) + l;
+        swap(arr, k, l);
+        int value = arr[l];
+
+        int j = l;
+
+        for (int i = l; i <= r; i++) {
+            if (arr[i] < value) {
+                swap(arr, j, i);
+                j++;
+            }
+        }
+
+        swap(arr, l, j);
+        return j;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{4,7,9,3,1,2,12,11,19,87,21,29,39,37,25,33,25};
+
+        new QuickSortStrategy().sort(arr);
+
+        for (int i : arr) {
+            System.out.print(i + ", ");
+
+        }
+    }
+}
---
+++ src/main/java/com/shengsi/design/pattern/behavioral/strategy/QuickSortStrategyV3.java
@@ -0,0 +1,52 @@
+package com.shengsi.design.pattern.behavioral.strategy;
+
+import java.util.Random;
+
+public class QuickSortStrategyV3 implements Strategy {
+
+    @Override
+    public void sort(int[] arr) {
+        quickSort(arr, 0, arr.length - 1);
+    }
+
+    private void quickSort(int[] arr, int l, int r) {
+        if (l >=r )
+            return;
+        int p = partition(arr, l, r);
+
+        quickSort(arr, 0, p-1);
+        quickSort(arr, p + 1, r);
+    }
+
+    private int partition(int[] arr, int l, int r) {
+        int k = new Random().nextInt(r - l + 1) + l;
+        swap(arr, k, l);
+        int value = arr[l];
+
+        int i = l + 1;
+        int j = r;
+
+        while(true) {
+            if (i > j) break;
+            while(i <= r && arr[i] < value) i++;
+            while(j >= l + 1 && arr[j] > value) j--;
+            swap(arr, i, j);
+            i++;
+            j--;
+        }
+
+        swap(arr, l, j);
+        return j;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{4,7,9,3,1,2,12,11,19,87,21,29,39,37,25,33,25, 25, 25, 26, 26, 26, 26, 26};
+
+        new QuickSortStrategy().sort(arr);
+
+        for (int i : arr) {
+            System.out.print(i + ", ");
+
+        }
+    }
+}
